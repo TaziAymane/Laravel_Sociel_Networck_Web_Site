cb289fe6d52b28739160aff5a179d25f883d0dec
Tazi Aymane <taziaymane68@gmail.com>
2025-05-06 15:28:51 +0100

    add publication setup

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -7,30 +7,31 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('homePage') }}">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('create') }}">Create Profile</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('profiles.index') }}">Tous les Profiles</a>
-                </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('seetings.index') }}">Settings</a>
-                </li>
                 @guest
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="{{ route('login.show') }}">Se Connect</a>
                     </li>
                 @endguest
-              
+
                 @auth
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('homePage') }}">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('create') }}">Create Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('profiles.index') }}">Tous les Profiles</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('seetings.index') }}">Settings</a>
+                    </li>
                     <div class="dropdown">
                         <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
                             data-bs-toggle="dropdown" aria-expanded="false">
-                           {{ auth()->user()->name}}
+                            {{ auth()->user()->name }}
                         </button>
                         <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                             <li><a class="dropdown-item" href="#">Action</a></li>
